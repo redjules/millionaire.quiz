5ff6a6456f7efd490f3ca8e55e11ec94b6b51c24
Ana Stone <106017493+redjules@users.noreply.github.com>
2023-04-10 14:46:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ function App() {
     },
     {
       id: 5,
-      question: "Who wrote `El Quijote?",
+      question: "Who wrote `El Quijote`?",
       answers: [
         {
           text: "Leonardo Da Vinci",
@@ -118,6 +118,226 @@ function App() {
         {
           text: "Miguel de Cervantes",
           correct: true,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "According to legend, what is at the end of a rainbow?",
+      answers: [
+        {
+          text: "Jesus",
+          correct: false,
+        },
+        {
+          text: "Elf",
+          correct: false,
+        },
+        {
+          text: "Pot of gold",
+          correct: true,
+        },
+        {
+          text: "Light",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Which of these is NOT a letter of the Greek alphabet?",
+      answers: [
+        {
+          text: "Alpha",
+          correct: false,
+        },
+        {
+          text: "Omega",
+          correct: false,
+        },
+        {
+          text: "Sigma",
+          correct: false,
+        },
+        {
+          text: "Umlaut",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Of the twelve signs of the Zodiac which one is known as 'The Archer'?",
+      answers: [
+        {
+          text: "Sagittarius",
+          correct: true,
+        },
+        {
+          text: "Aquarius",
+          correct: false,
+        },
+        {
+          text: "Capricorn",
+          correct: false,
+        },
+        {
+          text: "Virgo",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Which author created Sherlock Holmes?",
+      answers: [
+        {
+          text: "Agatha Christie",
+          correct: false,
+        },
+        {
+          text: "G.K. Chesterton",
+          correct: false,
+        },
+        {
+          text: "Sir Arthur Conan Doyle",
+          correct: true,
+        },
+        {
+          text: "Edgar Allan Poe",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Goulash is the national dish of which country?",
+      answers: [
+        {
+          text: "India",
+          correct: false,
+        },
+        {
+          text: "Greece",
+          correct: false,
+        },
+        {
+          text: "Spain",
+          correct: false,
+        },
+        {
+          text: "Hungary",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "What is the capital of Pakistan?",
+      answers: [
+        {
+          text: "Islamabad",
+          correct: true,
+        },
+        {
+          text: "Lahore",
+          correct: false,
+        },
+        {
+          text: "Karachi",
+          correct: false,
+        },
+        {
+          text: "Pasni",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "What does a tegestologist collect?",
+      answers: [
+        {
+          text: "Beer mats",
+          correct: true,
+        },
+        {
+          text: "Certificates",
+          correct: false,
+        },
+        {
+          text: "Bird's eggs",
+          correct: false,
+        },
+        {
+          text: "Walking sticks",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Which explorer discovered the island of Jamaica?",
+      answers: [
+        {
+          text: "Francis Drake",
+          correct: false,
+        },
+        {
+          text: "Christopher Columbus",
+          correct: true,
+        },
+        {
+          text: "Ferdinand Magellan",
+          correct: false,
+        },
+        {
+          text: "Vasco da Gama",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Stockholm, the capital of Sweden, is built on how many small islands which are connected by about 50 bridges?",
+      answers: [
+        {
+          text: "14",
+          correct: true,
+        },
+        {
+          text: "24",
+          correct: false,
+        },
+        {
+          text: "4",
+          correct: false,
+        },
+        {
+          text: "44",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "Dodge City is located in which American State?",
+      answers: [
+        {
+          text: "Montana",
+          correct: false,
+        },
+        {
+          text: "Utah",
+          correct: false,
+        },
+        {
+          text: "Kansas",
+          correct: true,
+        },
+        {
+          text: "Arizona",
+          correct: false,
         },
       ],
     },
