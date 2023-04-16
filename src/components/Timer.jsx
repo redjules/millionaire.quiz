39fcb24baab4f994dd002d071b9fef957e9d2efc
Ana P <anapedrabcn@gmail.com>
2023-04-17 01:04:25 +0200

    change

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -7,7 +7,7 @@ export default function Timer({ setTimeOut, questionNumber }) {
     if (timer === 0) return setTimeOut(true);
     const interval = setInterval(() => {
       setTimer((prev) => prev - 1);
-    }, 1000);
+    }, 500);
     return () => clearInterval(interval);
   }, [timer, setTimeOut]);
 
