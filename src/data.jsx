2172bc4c4227ec5b25ccfb481be3fc2a4124c401
Ana P <anapedrabcn@gmail.com>
2023-04-17 00:58:37 +0200

    Moved questions to different file

---
+++ src/data.jsx
@@ -0,0 +1,334 @@
+export const data = [
+  {
+    id: 1,
+    question: "Rolex is a company that specializes in what type of product?",
+    answers: [
+      {
+        text: "Phone",
+        correct: false,
+      },
+      {
+        text: "Watches",
+        correct: true,
+      },
+      {
+        text: "Food",
+        correct: false,
+      },
+      {
+        text: "Cosmetic",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "When did the website `Facebook` launch?",
+    answers: [
+      {
+        text: "2004",
+        correct: true,
+      },
+      {
+        text: "2005",
+        correct: false,
+      },
+      {
+        text: "2006",
+        correct: false,
+      },
+      {
+        text: "2007",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Who played the character of Harry Potter in movie?",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Leonardo Di Caprio",
+        correct: false,
+      },
+      {
+        text: "Denzel Washington",
+        correct: false,
+      },
+      {
+        text: "Daniel Radcliffe",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is the capital of France?",
+    answers: [
+      {
+        text: "Paris",
+        correct: true,
+      },
+      {
+        text: "Madrid",
+        correct: true,
+      },
+      {
+        text: "Dublin",
+        correct: false,
+      },
+      {
+        text: "London",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Who wrote `El Quijote`?",
+    answers: [
+      {
+        text: "Leonardo Da Vinci",
+        correct: false,
+      },
+      {
+        text: "Luis de Góngora",
+        correct: false,
+      },
+      {
+        text: "Arturo Pérez-Reverte",
+        correct: false,
+      },
+      {
+        text: "Miguel de Cervantes",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "According to the legend, what is at the end of a rainbow?",
+    answers: [
+      {
+        text: "Jesus",
+        correct: false,
+      },
+      {
+        text: "Elf",
+        correct: false,
+      },
+      {
+        text: "Pot of gold",
+        correct: true,
+      },
+      {
+        text: "Light",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "Which of these is NOT a letter of the Greek alphabet?",
+    answers: [
+      {
+        text: "Alpha",
+        correct: false,
+      },
+      {
+        text: "Omega",
+        correct: false,
+      },
+      {
+        text: "Sigma",
+        correct: false,
+      },
+      {
+        text: "Umlaut",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question:
+      "Of the twelve signs of the Zodiac which one is known as 'The Archer'?",
+    answers: [
+      {
+        text: "Sagittarius",
+        correct: true,
+      },
+      {
+        text: "Aquarius",
+        correct: false,
+      },
+      {
+        text: "Capricorn",
+        correct: false,
+      },
+      {
+        text: "Virgo",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Which author created Sherlock Holmes?",
+    answers: [
+      {
+        text: "Agatha Christie",
+        correct: false,
+      },
+      {
+        text: "G.K. Chesterton",
+        correct: false,
+      },
+      {
+        text: "Sir Arthur Conan Doyle",
+        correct: true,
+      },
+      {
+        text: "Edgar Allan Poe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "Goulash is the national dish of which country?",
+    answers: [
+      {
+        text: "India",
+        correct: false,
+      },
+      {
+        text: "Greece",
+        correct: false,
+      },
+      {
+        text: "Spain",
+        correct: false,
+      },
+      {
+        text: "Hungary",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "What is the capital of Pakistan?",
+    answers: [
+      {
+        text: "Islamabad",
+        correct: true,
+      },
+      {
+        text: "Lahore",
+        correct: false,
+      },
+      {
+        text: "Karachi",
+        correct: false,
+      },
+      {
+        text: "Pasni",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "What does a tegestologist collect?",
+    answers: [
+      {
+        text: "Beer mats",
+        correct: true,
+      },
+      {
+        text: "Certificates",
+        correct: false,
+      },
+      {
+        text: "Bird's eggs",
+        correct: false,
+      },
+      {
+        text: "Walking sticks",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "Which explorer discovered the island of Jamaica?",
+    answers: [
+      {
+        text: "Francis Drake",
+        correct: false,
+      },
+      {
+        text: "Christopher Columbus",
+        correct: true,
+      },
+      {
+        text: "Ferdinand Magellan",
+        correct: false,
+      },
+      {
+        text: "Vasco da Gama",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "Stockholm, the capital of Sweden, is built on how many small islands which are connected by about 50 bridges?",
+    answers: [
+      {
+        text: "14",
+        correct: true,
+      },
+      {
+        text: "24",
+        correct: false,
+      },
+      {
+        text: "4",
+        correct: false,
+      },
+      {
+        text: "44",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "Dodge City is located in which American State?",
+    answers: [
+      {
+        text: "Montana",
+        correct: false,
+      },
+      {
+        text: "Utah",
+        correct: false,
+      },
+      {
+        text: "Kansas",
+        correct: true,
+      },
+      {
+        text: "Arizona",
+        correct: false,
+      },
+    ],
+  },
+];
